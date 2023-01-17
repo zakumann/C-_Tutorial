92aa4870a79ae297f16998aef5c67ed6d26f240d
zakumann <samuel742able@gmail.com>
2023-01-17 19:56:53 +0900

    first commit

---
+++ Lesson/Lesson/CPP_Test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string first_name ;
+
+	cout << "Enter your first name: ";
+	cin >> first_name;
+	cout << "Hello " << first_name << "!" << endl;
+	cout << "Welcome!";
+
+	return 0;
+}
